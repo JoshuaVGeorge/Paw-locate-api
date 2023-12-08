79295a82f0ecf73f024993c80deace4a7f480851
Joshua George <joshua.vincent.george@gmail.com>
2023-12-07 23:33:56 -0800

    renamed usercontroller to signupcontroller

--- routes/signup.js
+++ routes/signup.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const usersController = require("../controllers/signup-controller");
+const signupController = require("../controllers/signup-controller");
 
-router.route("/").get(usersController.getAllUsers);
+router.route("/").get(signupController.getAllUsers);
 
 module.exports = router;
