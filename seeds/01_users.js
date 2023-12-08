0a9bdd51b5a75df21f2db38390d00131c6bf9f64
Joshua George <joshua.vincent.george@gmail.com>
2023-12-07 18:27:22 -0800

    username changed to user_name

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -6,9 +6,9 @@ exports.seed = async function (knex) {
 	// Deletes ALL existing entries
 	await knex("users").del();
 	await knex("users").insert([
-		{ id: 1, username: "pmatiebe0", password: "wC0!VaMHaXLWdT!" },
-		{ id: 2, username: "cphilbrick1", password: "bQ8~\\QQdyU0b4" },
-		{ id: 3, username: "swixey2", password: "vS7?CM*9/q?&f" },
-		{ id: 4, username: "etomkies3", password: "uU1}VPtq#n" },
+		{ id: 1, user_name: "pmatiebe0", password: "wC0!VaMHaXLWdT!" },
+		{ id: 2, user_name: "cphilbrick1", password: "bQ8~\\QQdyU0b4" },
+		{ id: 3, user_name: "swixey2", password: "vS7?CM*9/q?&f" },
+		{ id: 4, user_name: "etomkies3", password: "uU1}VPtq#n" },
 	]);
 };
