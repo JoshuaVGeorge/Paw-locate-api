3816bbd7b40ff05e1dba6506837f7106e4ff77f3
Joshua George <joshua.vincent.george@gmail.com>
2023-12-07 23:00:35 -0800

    implemented cors , express.json and port.env

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
-
 const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
 	res.send("welcome to paw-locate-api");
 });
 
-app.listen(8080, () => {
+app.listen(port, () => {
 	console.log("paw-locate-api launched");
 });
