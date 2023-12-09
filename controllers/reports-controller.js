7911d2a6d47c3568d1f1dfb1d31d0352137cdc4f
Joshua George <joshua.vincent.george@gmail.com>
2023-12-08 23:06:26 -0800

    createNewREport functionality added

--- controllers/reports-controller.js
+++ controllers/reports-controller.js
@@ -11,6 +11,41 @@ const getAllReports = (req, res) => {
 		});
 };
 
+const createNewReport = (req, res) => {
+	const {
+		user_id,
+		pet_name,
+		pet_image,
+		description,
+		contact_info,
+		location_data,
+	} = req.body;
+
+	if (
+		!user_id ||
+		!pet_name ||
+		!pet_image ||
+		!description ||
+		!contact_info ||
+		!location_data
+	) {
+		return res.status(400).send("Please make sure to fill all fields");
+	}
+
+	knex("reports")
+		.insert(req.body)
+		.then((result) => {
+			return knex("reports").where({ id: result[0] });
+		})
+		.then((newReport) => {
+			res.status(201).json(newReport);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: `${err}` });
+		});
+};
+
 module.exports = {
 	getAllReports,
+	createNewReport,
 };
