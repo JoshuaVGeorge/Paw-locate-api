37cbad452b2fdf78aecd14c4140efc016e576443
Joshua George <joshua.vincent.george@gmail.com>
2023-12-09 00:52:29 -0800

    getprofile & getProfileTips created

--- controllers/profile-controller.js
+++ controllers/profile-controller.js
@@ -1,8 +1,18 @@
 const knex = require("knex")(require("../knexfile"));
 
+const getProfile = (req, res) => {
+	knex("users as u")
+		.where({ "u.id": req.params.id })
+		.select("u.user_name")
+		.then((profile) => {
+			res.send(profile);
+		});
+};
+
 const getProfileReports = (req, res) => {
-	knex("reports")
-		.where({ user_id: req.params.id })
+	knex("reports as r")
+		.where({ "r.user_id": req.params.id })
+		.select("r.id", "r.pet_name", "r.status")
 		.then((reports) => {
 			if (reports.length === 0) {
 				res
@@ -17,6 +27,26 @@ const getProfileReports = (req, res) => {
 		});
 };
 
+const getProfileTips = (req, res) => {
+	knex("tips as t")
+		.where({ "t.user_id": req.params.id })
+		.select("t.id", "t.text_data", "t.status", "t.image")
+		.then((reports) => {
+			if (reports.length === 0) {
+				res
+					.status(404)
+					.json({ message: `no tips with user ID: ${req.params.id} exist` });
+			} else {
+				res.send(reports);
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ message: `${err}` });
+		});
+};
+
 module.exports = {
+	getProfile,
 	getProfileReports,
+	getProfileTips,
 };
