e325ec9648059ac04128a1b30a5d6dc45378de8c
Joshua George <joshua.vincent.george@gmail.com>
2023-12-08 22:35:38 -0800

    removed alias from getAllUsers

--- controllers/signup-controller.js
+++ controllers/signup-controller.js
@@ -2,7 +2,7 @@ const knex = require("knex")(require("../knexfile"));
 
 // just for testing . remove eventually
 const getAllUsers = (req, res) => {
-	knex("users as u")
+	knex("users")
 		.select("*")
 		.then((data) => {
 			res.status(200).send(data);
