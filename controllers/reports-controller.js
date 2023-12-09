f03880d5478442a29f11104741dd6720a40639fa
Joshua George <joshua.vincent.george@gmail.com>
2023-12-08 22:35:58 -0800

    getAllReports functionality added

--- controllers/reports-controller.js
+++ controllers/reports-controller.js
@@ -1,7 +1,14 @@
 const knex = require("knex")(require("../knexfile"));
 
 const getAllReports = (req, res) => {
-	res.send("welcome to reports");
+	knex("reports")
+		.select("*")
+		.then((data) => {
+			res.status(200).send(data);
+		})
+		.catch((err) => {
+			res.status(500).send(`${err}`);
+		});
 };
 
 module.exports = {
