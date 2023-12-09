3c33325ee4165061d85f7c33103aa5820f51ae69
Joshua George <joshua.vincent.george@gmail.com>
2023-12-09 00:05:58 -0800

    getProfileReports added

--- controllers/reports-controller.js
+++ controllers/reports-controller.js
@@ -81,8 +81,26 @@ const updateReport = (req, res) => {
 			res.status(500).json({ message: `${err}` });
 		});
 };
+
+const getProfileReports = (req, res) => {
+	knex("reports")
+		.where({ user_id: req.params.id })
+		.then((reports) => {
+			if (reports.length === 0) {
+				res
+					.status(404)
+					.json({ message: `no reports with user ID: ${req.params.id} exist` });
+			} else {
+				res.send(reports);
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ message: `${err}` });
+		});
+};
 module.exports = {
 	getAllReports,
 	createNewReport,
 	updateReport,
+	getProfileReports,
 };
