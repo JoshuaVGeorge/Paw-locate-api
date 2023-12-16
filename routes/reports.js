14b6054706e6c093470355e30ed2bc82f73d0553
Joshua George <joshua.vincent.george@gmail.com>
2023-12-16 05:28:02 -0800

    added multer middleware

--- routes/reports.js
+++ routes/reports.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
 const router = express.Router();
 const reportController = require("../controllers/reports-controller");
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, "./public/images");
+	},
+	filename: (req, file, cb) => {
+		cb(null, Date.now() + path.extname(file.originalname));
+	},
+});
+
+const upload = multer({ storage: storage });
 
 router
 	.route("/")
 	.get(reportController.getAllReports)
-	.post(reportController.createNewReport);
+	.post(upload.single("image"), reportController.createNewReport);
 
 router
 	.route("/:id")
